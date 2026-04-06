da32d7bcd6532d2877251b1185e79c109f2b8f9f
= <pritamach.exe@gmail.com>
2026-04-07 01:10:15 +0530

    disable redis in prod using profile

--- src/main/java/com/codecomp/codecomp/config/RedisListenerConfig.java
+++ src/main/java/com/codecomp/codecomp/config/RedisListenerConfig.java
@@ -2,6 +2,7 @@ package com.codecomp.codecomp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -11,9 +12,9 @@ import com.codecomp.codecomp.redis.RedisSubscriber;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@Profile("!prod")
 @RequiredArgsConstructor
 public class RedisListenerConfig {
-
     private final RedisSubscriber subscriber;
 
     @Bean
